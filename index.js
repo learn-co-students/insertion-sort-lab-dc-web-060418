0097e8eba472a33fe81fc79af0d395aaf4bef47a
Susanna Kosonen <susannakosonen@Susannas-MacBook-Pro.local>
2018-09-19 14:46:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 function findMinAndRemove(array){
-
+    let min = array[0]
+    for(let i = 0; i < array.length; i++) {
+        if(min>array[i]) {
+            min = array[i]
+        }
+    }
+    let minI = array.indexOf(min)
+    array.splice(minI, 1)
+    return min
 }
 
 function insertionSort(array){
-
+    let sortedArray = []
+    while(array.length !== 0) {
+        //find the minimum value, push it to the sortedArray
+        //also modify original array by removing the min value
+        sortedArray.push(findMinAndRemove(array))
+    }
+    return sortedArray
 }
